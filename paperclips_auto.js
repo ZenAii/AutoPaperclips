848df5212484a44f8aa8cd179cc3cd2e67a7c440
ZenAii <anthonyroulois506@gmail.com>
2025-01-09 18:57:00 +0100

    Parametres

---
+++ paperclips_auto.js
@@ -0,0 +1,12 @@
+(() => {
+  if (window.__paperclips_auto === true) return;
+  window.__paperclips_auto = true;
+
+  let cache = {};
+
+  const AcceptOffer = false;
+  const MaxMemory = 250;
+  const MemoryToProcessorsRatio = 2;
+  const PreferredStrategyIndex = 4;
+  const SwarmComputingLevel = 150;
+})();
